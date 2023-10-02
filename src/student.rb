1da48faedca56bd765575215b43db6258b5cddd3
Fatima Najafi <najafifatima762@gmail.com>
2023-10-02 17:00:03 +0430

    Create the student.rb

---
+++ src/student.rb
@@ -0,0 +1,12 @@
+# require_relative 'Person'
+
+# class Student < Person
+#   def initialize(name, age, classroom)
+#     super(name, age)
+#     @classroom = classroom
+#   end
+
+#   def play_hooky
+#     '¯\(ツ)/¯'
+#   end
+# end
