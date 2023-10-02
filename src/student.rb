583d8539309c139de158353ef8c66ad46817e9a0
Fatima Najafi <najafifatima762@gmail.com>
2023-10-02 17:01:30 +0430

    Create the student class

--- src/student.rb
+++ src/student.rb
@@ -1,12 +1,12 @@
-# require_relative 'Person'
+require_relative 'Person'
 
-# class Student < Person
-#   def initialize(name, age, classroom)
-#     super(name, age)
-#     @classroom = classroom
-#   end
+class Student < Person
+  def initialize(name, age, classroom)
+    super(name, age)
+    @classroom = classroom
+  end
 
-#   def play_hooky
-#     '¯\(ツ)/¯'
-#   end
-# end
+  def play_hooky
+    '¯\(ツ)/¯'
+  end
+end
