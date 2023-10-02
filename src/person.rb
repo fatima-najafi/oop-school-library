36681157133d6caf903451a96e9df5b90bcfac90
Fatima Najafi <najafifatima762@gmail.com>
2023-10-02 17:01:03 +0430

    Create the person class

--- src/person.rb
+++ src/person.rb
@@ -1,25 +1,25 @@
-# class Person
-#   def initialize(name = 'Unknown', age = 0, parent_permission: true)
-#     @id = Random.rand(1..1000)
-#     @name = name
-#     @age = age
-#     @parent_permission = parent_permission
-#   end
+class Person
+  def initialize(name = 'Unknown', age = 0, parent_permission: true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
 
-#   attr_accessor :name, :age
-#   attr_reader :id
+  attr_accessor :name, :age
+  attr_reader :id
 
-#   def can_use_services?
-#     of_age || @parent_permission
-#   end
+  def can_use_services?
+    of_age || @parent_permission
+  end
 
-#   private
+  private
 
-#   def of_age?
-#     @age >= 18
-#   end
+  def of_age?
+    @age >= 18
+  end
 
-#   def validate_name
-#     @name = 'Unknown' if @name == ''
-#   end
-# end
+  def validate_name
+    @name = 'Unknown' if @name == ''
+  end
+end
