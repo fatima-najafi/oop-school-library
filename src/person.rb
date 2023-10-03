0a7682d249616b9d64dff079bab4afca3b2d0f5e
Fatima Najafi <najafifatima762@gmail.com>
2023-10-03 19:26:07 +0430

    Add correct_name method

--- src/person.rb
+++ src/person.rb
@@ -1,35 +1,36 @@
- require_relative './nameable'
- require_relative  './capitalize_decorator'
- require_relative  './trimmer_decorator'
- require_relative './decorator'
+require_relative 'nameable'
+require_relative 'capitalize_decorator'
+require_relative 'trimmer_decorator'
+require_relative 'decorator'
 
 class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
-  def initialize(name = 'Unknown', age,  parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
-  def correct_name 
+  def correct_name
     @name
-    
   end
 
   def can_use_services?
     of_age? || @parent_permission
   end
 
-  private 
+  private
+
   def of_age?
     @age >= 18
   end
 end
 
-person = Person.new('maximilianus', 22)
+person = Person.new(22, 'maximilianus')
 person.correct_name
 capitalized_person = CapitalizeDecorator.new(person)
 puts capitalized_person.correct_name
