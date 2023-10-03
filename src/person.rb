bc848a5a2cd21382a2b278c8669028271499b5be
Fatima Najafi <najafifatima762@gmail.com>
2023-10-03 19:17:32 +0430

    Write the code for testing

--- src/person.rb
+++ src/person.rb
@@ -1,9 +1,13 @@
- require './nameable'
+ require_relative './nameable'
+ require_relative  './capitalize_decorator'
+ require_relative  './trimmer_decorator'
+ require_relative './decorator'
+
 class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
-  def initialize(age, name = 'Unknown', parent_permission: true)
+  def initialize(name = 'Unknown', age,  parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -19,15 +23,15 @@ class Person < Nameable
     of_age? || @parent_permission
   end
 
-  private
-
+  private 
   def of_age?
     @age >= 18
   end
-
-  def validate_name
-    @name = 'Unknown' if @name == ''
-  end
-
-
 end
+
+person = Person.new('maximilianus', 22)
+person.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
