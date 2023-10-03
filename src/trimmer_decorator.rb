02c6d204e36ee457cfa0d0d12297f36360cd8134
Fatima Najafi <najafifatima762@gmail.com>
2023-10-03 18:43:29 +0430

    Create the trimmer_decorator file

---
+++ src/trimmer_decorator.rb
@@ -0,0 +1,9 @@
+require './decorator'
+class TrimmerDecorator < Decorator
+    def correct_name
+        name= @nameable.correct_name
+        name.length > 10 ? name[0..9] : name
+        
+    end
+    
+end
