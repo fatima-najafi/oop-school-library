5a1187b7c82ffd9f539ae7f5210a613a1d8411e7
Fatima Najafi <najafifatima762@gmail.com>
2023-10-04 16:45:56 +0430

    Update the person.rb

--- src/person.rb
+++ src/person.rb
@@ -4,7 +4,7 @@ require_relative 'trimmer_decorator'
 require_relative 'decorator'
 
 class Person < Nameable
-  attr_accessor :name, :age
+  attr_accessor :name, :age, :rentals
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
@@ -13,6 +13,11 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
+  end
+
+  def add_rental(date, book)
+    Rental.new(date, book, self)
   end
 
   def correct_name
