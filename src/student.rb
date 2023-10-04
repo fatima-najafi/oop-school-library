8af53139b6510746aba5381884e1771ebd39cc56
Fatima Najafi <najafifatima762@gmail.com>
2023-10-04 16:33:00 +0430

    Add belongs to relationship

--- src/student.rb
+++ src/student.rb
@@ -1,12 +1,19 @@
 require_relative 'person'
-
+  attr_reader :classroom
 class Student < Person
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
   end
 
+    def classroom=(classroom)
+    @classroom = classroom
+    classroom.add_student(self) unless classroom.students.include?(self)
+  end
+
   def play_hooky
     '¯\(ツ)/¯'
   end
 end
+
+
