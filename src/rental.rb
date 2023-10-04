8e2196395d27789366c379e13b4e69fdc5042891
Fatima Najafi <najafifatima762@gmail.com>
2023-10-04 16:24:01 +0430

    Update

--- src/rental.rb
+++ src/rental.rb
@@ -1,13 +1,12 @@
 class Rental
-     attr_accessor :date
-     attr_reader :book, :person
-    def initialize(date, book, person)
-        @date= date
-        @book= book
-        book.rentals << self
-        @person = person
-        person.rentals << self
-        
-    end
-    
+  attr_accessor :date
+  attr_reader :book, :person
+
+  def initialize(date, book, person)
+    @date = date
+    @book = book
+    book.rentals << self
+    @person = person
+    person.rentals << self
+  end
 end
