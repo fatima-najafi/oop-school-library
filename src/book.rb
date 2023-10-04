fad46ed7254cf9901e688820ec23605882cfe5e0
Fatima Najafi <najafifatima762@gmail.com>
2023-10-04 16:22:05 +0430

    Add relationship

--- src/book.rb
+++ src/book.rb
@@ -1,17 +1,14 @@
 class Book
-    attr_accessor :title, :author, :rentals
+  attr_accessor :title, :author, :rentals
 
 
-    def initialize(title, author)
-        @title = title
-        @author = author
-        @rentals = []
-        
-    end
+  def initialize(title, author)
+    @title = title
+    @author = author
+    @rentals = []
+  end
 
-    def add_rental(date, person)
-        Rental.new(date, self, person)
-        
-    end
-    
+  def add_rental(date, person)
+    Rental.new(date, self, person)
+  end
 end
