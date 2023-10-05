6a4ecf0f0cf7bf59477fe8fbee2d5ff98e79ca42
Fatima Najafi <najafifatima762@gmail.com>
2023-10-05 14:33:01 +0430

    Update the person.rb

--- src/person.rb
+++ src/person.rb
@@ -35,9 +35,9 @@ class Person < Nameable
   end
 end
 
-person = Person.new(22, 'maximilianus')
-person.correct_name
-capitalized_person = CapitalizeDecorator.new(person)
-puts capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts capitalized_trimmed_person.correct_name
+# person = Person.new(22, 'maximilianus')
+# person.correct_name
+# capitalized_person = CapitalizeDecorator.new(person)
+# puts capitalized_person.correct_name
+# capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+# puts capitalized_trimmed_person.correct_name
