969c4fa5f1f6d792fbef15e3a2ec2f585a9f82a1
Fatima Najafi <najafifatima762@gmail.com>
2023-10-05 22:16:48 +0430

    Implement the changes

--- src/main.rb
+++ src/main.rb
@@ -1,45 +1,41 @@
-require_relative 'app'
 
-def display
-  puts 'Please choose an option by entering a number:'
-  puts '1 - List all books'
-  puts '2 - List all people'
-  puts '3 - Create a person'
-  puts '4 - Create a book'
-  puts '5 - Create a rental'
-  puts '6 - List all rentals for a given person id'
-  puts '7 - Exit'
-end
+require_relative './app'
 
-def run_app(app)
-  menu_options = {
-    '1' => :list_books,
-    '2' => :list_people,
-    '3' => :create_person,
-    '4' => :create_book,
-    '5' => :create_rental,
-    '6' => :list_rentals,
-    '7' => :exit
-  }
-  loop do
-    display
-    input = gets.chomp
-
-    if menu_options.key?(input)
-      run = menu_options[input]
-      break if run == :exit
-
-      app.send(run)
-    else
-      puts 'Enter the correct option: '
-    end
+puts "Welcome to School Library App!\n\n"
+MENU = "Please choose an option by entering a number:\n\
+1 - List all books\n\
+2 - List all people \n\
+3 - Create a person\n\
+4 - Create a book\n\
+5 - Create a rental\n\
+6 - List all rentals for a given person ID\n\
+7 - Exit".freeze
+APP = App.new
+def switch(get)
+  case get
+  when 1
+    APP.list_books
+  when 2
+    APP.list_people
+  when 3
+    APP.create_person
+  when 4
+    APP.create_book
+  when 5
+    APP.create_rental
+  when 6
+    APP.list_rentals
   end
 end
 
 def main
-  app = App.new
-  puts 'Welcome to the School Library App!'
-  run_app(app)
+  get = 0
+  while get != 7 and get < 7
+    puts MENU
+    get = gets.chomp.to_i
+    switch(get)
+  end
+  puts 'Thank you for using this app!'
 end
 
 main
