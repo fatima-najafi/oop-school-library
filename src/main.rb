7a03bad1093a2b26e1a1c765e2d2d6ccd20aeb17
Fatima Najafi <najafifatima762@gmail.com>
2023-10-05 14:32:03 +0430

    Present the user with a list of options to perform

---
+++ src/main.rb
@@ -0,0 +1,59 @@
+ require_relative 'app'
+
+def display
+  puts 'Please choose an option by entering a number:'
+  puts '1 - List all books'
+  puts '2 - List all people'
+  puts '3 - Create a person'
+  puts '4 - Create a book'
+  puts '5 - Create a rental'
+  puts '6 - List all rentals for a given person id'
+  puts '7 - Exit'
+end
+
+def run_app(app)
+  menu_options = {
+    '1' => :list_books,
+    '2' => :list_people,
+    '3' => :create_person,
+    '4' => :create_book,
+    '5' => :create_rental,
+    '6' => :list_rentals,
+    '7' => :exit
+  }
+  loop do
+    display
+    input = gets.chomp
+
+        if menu_options.key?(input)
+      run = menu_options[input]
+      break if run == :exit
+      app.send(run)
+    else
+      puts 'Enter the correct option: '
+    end
+  end
+
+  puts 'Thank you for using my app!'
+end
+
+#     if menu_options.key?(input)
+#       run = menu_options[input]
+#       app.send(run)
+#       break if run == :exit
+#     else
+#       puts 'Enter the correct option: '
+#     end
+#   end
+
+#     puts 'Thank you for using my app!'
+# end
+
+def main
+  app = App.new
+  puts 'Welcome to the School Library App!'
+  run_app(app)
+end
+
+main
+
